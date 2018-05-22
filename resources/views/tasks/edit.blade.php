d13103203d333e3c243501ac0fd3ea7d2effb44e
EC2 Default User <ec2-user@ip-172-31-24-157.us-east-2.compute.internal>
2018-05-22 07:56:49 +0000

    bootstrap

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,19 +4,27 @@
 
 <!-- ここにページ毎のコンテンツを書く -->
 
+<div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+
+
 <h1>id: {{ $task->id }} の編集ページ</h1>
-
-
 
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
+    <div class="form-group">
+
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+    </div>
 
+    <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+    </div>
 
-        {!! Form::submit('更新') !!}
+
+        {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
 
